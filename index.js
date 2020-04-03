0d33b856a5874d10bc3e29a5f938c04397c326e8
Reza <rzgholizadeh@gmail.com>
2020-04-02 19:36:23 -0600

    feat: Implement finding all test files

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log("Hello world");
+
+const Runner = require("./runner");
+const runner = new Runner();
+
+const run = async () => {
+	const results = await runner.collectFiles(process.cwd());
+	console.log(results);
+};
+
+run();
